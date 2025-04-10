271543e3ef4e43990d07d7afd7f6d83a26e6f93c
Bilallemar <112867585+Bilallemar@users.noreply.github.com>
2025-04-10 11:21:27 +0430

    new crud operation

---
+++ NewCRUD/Services/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NewCRUD.Models;
+
+namespace NewCRUD.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+       public DbSet<Product> Products {  get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+                new Product { Id = 1, Name = "Lapstic", Brand = "hh", Category = "gg", Price = 44, Description = "llll", ImageFileName = "1.jpg" }
+               
+                );
+        }
+    }
+
+}
+
